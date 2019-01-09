78131705b700835ef9b62dcef8b0dbf6e2382ebe
Diuda <divyanshu.1996@gmail.com>
2019-01-09 13:57:35 +0530

    added /product route

--- routes/api.js
+++ routes/api.js
@@ -7,5 +7,18 @@ router.get('/', (req, res) => {
     })
 })
 
+var productController = require('../controller/productController');
+
+
+router.route('/products')
+    .get(productController.index)
+    .post(productController.new)
+
+router.route('/products/:pid')
+    .get(productController.view)
+    .patch(productController.update)
+    .put(productController.update)
+    .delete(productController.delete)
+
 
 module.exports = router;
