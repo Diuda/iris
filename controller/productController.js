d0a93eb0d266438e46850137a48d3a8ce0041a85
Diuda <divyanshu.1996@gmail.com>
2019-01-09 13:33:45 +0530

    created view and new product added controller

--- controller/productController.js
+++ controller/productController.js
@@ -2,8 +2,8 @@ Product = require('../model/productModel');
 
 
 exports.index = (req, res) => {
-    Product.get((err, products)=>{
-        if(err) {
+    Product.get((err, products) => {
+        if (err) {
             res.json({
                 status: "error",
                 message: err
@@ -11,7 +11,7 @@ exports.index = (req, res) => {
         }
 
         res.json({
-            status:"success",
+            status: "success",
             message: "Contact retrieved successfully",
             data: products
         });
@@ -19,7 +19,37 @@ exports.index = (req, res) => {
 };
 
 
-// exports.new = (req, res) => {
-//     var product = new Product();
-//     product.name = 
-// }
+exports.new = (req, res) => {
+    var product = new Product();
+    product.name = req.body.name ? req.body.name : product.name;
+    product.id = req.body.id;
+    product.category = req.body.category;
+    product.price = req.body.price;
+    product.color = req.body.color;
+
+
+    product.save((err) => {
+        if (err)
+            res.json(err)
+
+
+        res.json({
+            message: "New Product Created",
+            data: product
+
+        });
+    });
+};
+
+
+exports.view = (req, res) => {
+    Product.findById(req.params.product_id, (err, product){
+        if(err)
+            res.send(err);
+        res.json({
+            message: 'Products loading...',
+            data: product
+        });
+    });
+};
+
