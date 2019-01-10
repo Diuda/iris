e19433bcb613f5092dd08f0caed27e124aa0a6b2
Diuda <divyanshu.1996@gmail.com>
2019-01-10 12:28:44 +0530

    fixed product array issue

--- controller/productController.js
+++ controller/productController.js
@@ -58,16 +58,17 @@ exports.view = (req, res) => {
 //TODO fix update function
 exports.update = (req, res) => {
     Product.find({pid: req.params.pid}, (err, product) => {
+        console.log(product[0])
         if (err)
             res.send(err);
-        product.name = req.body.name ;
-        product.pid = req.body.pid;
-        product.category = req.body.category;
-        product.price = req.body.price;
-        product.color = req.body.color;
+        product[0].name = req.body.name ? req.body.name: product[0].name;
+        product[0].pid = req.body.pid ? req.body.pid: product[0].pid;
+        product[0].category = req.body.category ? req.body.category: product[0].category;
+        product[0].price = req.body.price ? req.body.price: product[0].price;
+        product[0].color = req.body.color ? req.body.color: product[0].color;
         console.log(product);
 
-        product.save((err)=>{
+        product[0].save((err)=>{
             if(err)
                 res.json(err);
             res.json({
